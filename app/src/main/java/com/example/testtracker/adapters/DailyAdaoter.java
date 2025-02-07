7788fd820fe3f45b04f2bb3637079ebe20cd7954
nour <n01555251740@gmail.com>
2025-02-08 01:58:02 +0200

    add daily meal

--- app/src/main/java/com/example/testtracker/adapters/DailyAdaoter.java
+++ app/src/main/java/com/example/testtracker/adapters/DailyAdaoter.java
@@ -1,4 +1,58 @@
 package com.example.testtracker.adapters;
 
-public class DailyAdaoter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.testtracker.R;
+import com.example.testtracker.network.Meal;
+
+import java.util.List;
+
+public class DailyAdaoter extends RecyclerView.Adapter<DailyAdaoter.MealViewHolder> {
+    private final static String TAG = "productClient";
+    private final Context context;
+    private final List<Meal> dailyList;
+
+    public DailyAdaoter(List<Meal> dailyList, Context context) {
+        this.dailyList = dailyList;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public MealViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.day_meal_card, parent, false);
+        return new MealViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MealViewHolder holder, int position) {
+        Meal meal = dailyList.get(position);
+        holder.name.setText(meal.getStrMeal());
+        Glide.with(context).load(meal.getStrMealThumb()).into(holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dailyList.size();
+    }
+    public static class MealViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        ImageView image;
+
+        public MealViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.mealName);
+            image = itemView.findViewById(R.id.mealImage);
+        }
+    }
 }
